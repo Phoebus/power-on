139edc28967c086f83d1097fab4eb49615256e97
Phoebus <phoebuspetsi@gmail.com>
2026-05-08 23:18:09 +0300

    remade (a bit) the part cards. the price is displayed in a nicer way which doesnt take up space

--- pc_parts/pc_part.gd
+++ pc_parts/pc_part.gd
@@ -5,8 +5,8 @@ class_name PcPart
 
 @onready var card_image : TextureRect = $Panel/VBoxContainer/Image
 @onready var card_name : Label = $Panel/VBoxContainer/Name
-@onready var card_description : RichTextLabel = $Panel/VBoxContainer/Description
-@onready var card_price : Label = $Panel/VBoxContainer/Price
+# @onready var card_description : RichTextLabel = $Panel/VBoxContainer/Description
+#@onready var card_price : Label = $Panel/VBoxContainer/Price
 @onready var card_key_specs : RichTextLabel = $Panel/VBoxContainer/KeySpecs
 
 # Called when the node enters the scene tree for the first time.
@@ -20,6 +20,6 @@ func _process(_delta: float) -> void:
 func card_init() -> void:
 	card_image.texture = data.image
 	card_name.text = data.name
-	card_description.text = data.description
-	card_price.text = str(data.price)
+	# card_description.text = data.description
+	#card_price.text = str(data.price)
 	
