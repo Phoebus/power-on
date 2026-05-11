d69a5a9b2dd466c02ec7b59d6ccb002ea63a0dde
Phoebus <phoebuspetsi@gmail.com>
2026-05-12 00:54:20 +0300

    worked on the cart popup menu. you can now add and remove items from it.

--- shop_menu/cart.gd
+++ shop_menu/cart.gd
@@ -3,7 +3,7 @@ extends Panel
 # Idk if this is correct but i am storing this here for now
 var part_list_part_min_x : float = 700.0
 var part_list_part_min_y : float = 70.0
-var in_cart_part_row : PackedScene = preload("res://shop_menu/in_cart_part_row.tscn")
+var in_cart_part_row : PackedScene = load("res://shop_menu/in_cart_part_row.tscn")
 
 var selected_parts : Array[PcPartResource]
 
@@ -16,12 +16,15 @@ func _ready() -> void:
 func add_part(part_data : PcPartResource) -> void:
 	if not selected_parts.has(part_data):
 		selected_parts.append(part_data)
-		var new_item_row : Node = in_cart_part_row.instantiate()
+		var new_item_row : CartPartRow = in_cart_part_row.instantiate().with_data(part_data)
 		part_list.add_child(new_item_row)
 		new_item_row.custom_minimum_size = Vector2(part_list_part_min_x, part_list_part_min_y)
-		
+		new_item_row.remove_item.connect(_on_item_removal)	
 
 func _on_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed():
 			popup.show()
+
+func _on_item_removal(part_data : PcPartResource) -> void:
+	selected_parts.erase(part_data)
--- shop_menu/in_cart_part_row.gd
+++ shop_menu/in_cart_part_row.gd
@@ -1,9 +1,20 @@
+class_name CartPartRow
 extends HBoxContainer
+
+signal remove_item(data : PcPartResource)
 
 var part_data : PcPartResource
 
 @onready var data_label : Label = $Label
 
+func with_data(data : PcPartResource) -> CartPartRow:
+	part_data = data
+	return self
+
 func _ready() -> void:
-	#data_label.text = part_data.name
+	data_label.text = part_data.name
 	pass
+
+func _on_button_pressed() -> void:
+	remove_item.emit(part_data)
+	queue_free()
